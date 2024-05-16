728adf2c04abd6a90db13f5a13d45725a4fc90e2
Apichat Peebanmai <apichatlpg@gmail.com>
2024-05-16 16:01:15 +0700

    Project1 Finesh

--- lib/screen/my_second_page.dart
+++ lib/screen/my_second_page.dart
@@ -1,8 +1,9 @@
+import 'package:example_1/screen/my_home_page.dart';
 import 'package:example_1/screen/my_second_page.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MySecondPage extends StatelessWidget {
+  const MySecondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,27 +12,27 @@ class MyHomePage extends StatelessWidget {
         child: Center(
           child: Container(
             child: Text(
-              "Main Menu",
+              "Main Menu 2",
             ),
           ),
         ),
       ),
       appBar: AppBar(
-        title: Text("My Home Page Apichat Peebanmai"),
+        title: Text("My Home Page2"),
       ),
       body: Center(
           child: Column(
         children: [
-          Text("Hello World"),
+          Text("Hello World 2"),
           ElevatedButton(
             onPressed: () {
-              Navigator.of(context).push(
+              Navigator.of(context).pop(
                 MaterialPageRoute(
-                  builder: (context) => MySecondPage(),
+                  builder: (context) => MyHomePage(),
                 ),
               );
             },
-            child: Text("ไปยังหน้า 2"),
+            child: Text("ไปยังหน้า Home"),
           ),
         ],
       )),
